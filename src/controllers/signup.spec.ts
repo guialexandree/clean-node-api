f03934b0667b4182f8f469afaeb495ad94857615
guialexandre <guilherme_alexandree@hotmail.com>
2022-08-28 17:35:54 -0300

    feat: ensure SingUpController return status 400 if no name provided

---
+++ src/controllers/signup.spec.ts
@@ -0,0 +1,19 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+	test('Should return status 400 if no name is provided', () => {
+		const sut = new SignUpController()
+		const httpRequest = {
+			body: {
+				email: 'email@email.com',
+				password: 'any_password',
+				passwordConfirmation: 'any_password'
+			}
+		}
+
+		const httpResponse = sut.handle(httpRequest)
+
+		expect(httpResponse.statusCode).toBe(400)
+		expect(httpResponse.body).toEqual(new Error('Missing param name'))
+	})
+})
