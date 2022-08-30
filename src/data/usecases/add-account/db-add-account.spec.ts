ab90c954fced59cf8c3b75343ded9b488de89410
guialexandre <guilherme_alexandree@hotmail.com>
2022-08-29 23:00:13 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -103,4 +103,22 @@ describe('DbAddAccount Usecase', () => {
 
 		expect(promise).rejects.toThrow()
 	})
+
+	test('Should return an account on success', async () => {
+		const { sut } = makeSut()
+		const accountData = {
+			name: 'valid_name',
+			email: 'valid_email',
+			password: 'valid_password'
+		}
+
+		const account = await sut.add(accountData)
+
+		expect(account).toEqual({
+			id: 'valid_id',
+			name: 'valid_name',
+			email: 'valid_email',
+			password: 'hashed_password'
+		})
+	})
 })
