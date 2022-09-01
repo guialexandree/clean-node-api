d93c9011969f0d80058063095782bb7525b084a2
guialexandre <guilherme_alexandree@hotmail.com>
2022-08-31 21:59:38 -0300

    feat: ensure BcrypAdapter returns a to hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapater } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+	async hash (): Promise<string> {
+		return new Promise(resolve => resolve('any_hash'))
+	}
+}))
+
 describe('Bcrypt Adapter', () => {
 	const makeSut = () => {
 		const sut = new BcryptAdapater()
@@ -14,5 +20,13 @@ describe('Bcrypt Adapter', () => {
 		await sut.encrypt('any_value')
 
 		expect(hashSpy).toHaveBeenCalledWith('any_value', sut.salt)
+	})
+
+	test('Should returns a to hash on success', async () => {
+		const { sut } = makeSut()
+
+		const hash = await sut.encrypt('any_value')
+
+		expect(hash).toBe('any_hash')
 	})
 })
