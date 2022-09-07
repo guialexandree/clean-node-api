259428f2a2a77fa60ffc06514e954581503f603e
guialexandre <guilherme_alexandree@hotmail.com>
2022-09-07 15:20:36 -0300

    chore: update configs jest and mongodb

--- jest-unit-config.js
+++ jest-unit-config.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 /*
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
-module.exports = {
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-	testMatch: ['**/*.spec.ts'],
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
-=======
 const config = require('./jest.config')
 config.testMatch = ['**/*.spec.ts']
+
 module.exports = config
->>>>>>> 7144a61ebaac2ed211e717b70f709111a6ba2914
