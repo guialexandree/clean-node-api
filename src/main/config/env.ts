f06698b06e4dd178fdbc7ed8dc889f7b3b96f118
guialexandre <guilherme_alexandree@hotmail.com>
2022-09-08 21:31:39 -0300

    feat: add dotenv and env file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,6 @@
+require('dotenv').config()
+
 export default {
-	mongoUrl: process.env.MONGO_URL || 'mongodb+srv://guilhermealexandree:pUP2FGP6hlgRcaaV@cluster0.ojtzqyy.mongodb.net/?retryWrites=true&w=majority',
-	port: process.env.PORT || '5050'
+	mongoUrl: process.env.MONGO_URL,
+	port: process.env.MONGO_PORT
 }
