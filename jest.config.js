769ae26ec083efdeecdf7f08d63a1b594da124ad
guialexandre <guilherme_alexandree@hotmail.com>
2022-09-10 12:03:42 -0300

    test: ensure mongodb reconnect if client is down

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,12 @@
 
 module.exports = {
 	roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+		'<rootDir>/src/**/*.ts',
+		'!<rootDir>/src/main/**',
+		'!<rootDir>/src/**/*protocols.ts',
+		'!<rootDir>/src/**/*protocols/index.ts'
+	],
   coverageDirectory: 'coverage',
 	preset: '@shelf/jest-mongodb',
 	watchPathIgnorePatterns: ['globalConfig'],
---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,20 @@
+import { MongoHelper as sut } from './mongo-helpers'
+
+describe('Mongo Helper', () => {
+	beforeAll(async () => {
+		await sut.connect(global.__MONGO_URI__)
+	})
+
+	afterAll(async () => {
+		await sut.disconnect()
+	})
+	test('Should reconnect if mongodb is down', async () => {
+		let accountCollection = await sut.getCollection('accounts')
+		expect(accountCollection).toBeTruthy()
+
+		await sut.disconnect()
+
+		accountCollection = await sut.getCollection('accounts')
+		expect(accountCollection).toBeTruthy()
+	})
+})
