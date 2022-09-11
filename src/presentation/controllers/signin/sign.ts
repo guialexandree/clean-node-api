68dee4fc653f0412b456bc5fd5022a934c94d522
guialexandre <guilherme_alexandree@hotmail.com>
2022-09-11 14:36:21 -0300

    feat: ensure SignUpController returns 400 if password not provided

--- src/presentation/controllers/signin/sign.ts
+++ src/presentation/controllers/signin/sign.ts
@@ -1,9 +1,19 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, ok } from '../../helpers/http-helper'
 
 export class SignInController implements Controller {
 	async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-		return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+		const { email } = httpRequest.body
+		if (!email) {
+			return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+		}
+
+		const { password } = httpRequest.body
+		if (!password) {
+			return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
+		}
+
+		return new Promise(resolve => resolve(ok({ ok: 'ok' })))
 	}
 }
