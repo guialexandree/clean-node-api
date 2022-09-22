7eef6641f27411b3cf17a83c612ec85649cfd9ef
guialexandre <guilherme_alexandree@hotmail.com>
2022-09-21 22:09:32 -0300

    test: ensure JwtAdapter return a token on sign success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+	async sign (): Promise<string> {
+		return new Promise(resolve => resolve('any_token'))
+	}
+}))
+
 const makeSut = () : JwtAdapter => {
 	const sut = new JwtAdapter('any_secret')
 	return sut
@@ -14,5 +20,13 @@ describe('JWT Adapter', () => {
 		await sut.encrypt('any_id')
 
 		expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'any_secret')
+	})
+
+	test('Should returns a token on sign success', async () => {
+		const sut = makeSut()
+
+		const accessToken = await sut.encrypt('any_id')
+
+		expect(accessToken).toBe('any_token')
 	})
 })
