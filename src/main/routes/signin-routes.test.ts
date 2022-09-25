157099a386c42d00bc9322340374680c6fd58ee9
guialexandre <guilherme_alexandree@hotmail.com>
2022-09-25 12:29:20 -0300

    test: add singin route

--- src/main/routes/signin-routes.test.ts
+++ src/main/routes/signin-routes.test.ts
@@ -1,5 +1,9 @@
 import request from 'supertest'
+import { Collection } from 'mongodb'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('SignUp Routes', () => {
 	const app = require('../config/app').default
@@ -13,7 +17,7 @@ describe('SignUp Routes', () => {
 	})
 
 	beforeEach(async () => {
-		const accountCollection = await MongoHelper.getCollection('accounts')
+		accountCollection = await MongoHelper.getCollection('accounts')
 		accountCollection.deleteMany({})
 	})
 
@@ -26,6 +30,24 @@ describe('SignUp Routes', () => {
 					email: 'guilherme_alexandre@hotmail.com',
 					password: 'any_password',
 					passwordConfirmation: 'any_password'
+				})
+				.expect(200)
+		})
+	})
+
+	describe('POST /signin', () => {
+		test('Should return 200 on signin', async () => {
+			const password = await hash('123', 12)
+			accountCollection.insertOne({
+				name: 'Guilherme Alexandre',
+				email: 'guilherme_alexandre@hotmail.com',
+				password
+			})
+			await request(app)
+				.post('/api/signin')
+				.send({
+					email: 'guilherme_alexandre@hotmail.com',
+					password: '123'
 				})
 				.expect(200)
 		})
