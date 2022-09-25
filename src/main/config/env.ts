5e2edb2d0a179728eb1e99555dd70df23b9a2a48
guialexandre <guilherme_alexandree@hotmail.com>
2022-09-25 12:23:42 -0300

    feat: add SignIn Factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ require('dotenv').config()
 
 export default {
 	mongoUrl: process.env.MONGO_URL || 'localhost:270',
-	port: process.env.MONGO_PORT || 5050
+	port: process.env.MONGO_PORT || 5050,
+	jwtSecret: process.env.JWT_SECRET || 'Tl@s13s.'
 }
--- src/main/factories/signin/signin-factory.ts
+++ src/main/factories/signin/signin-factory.ts
@@ -1,18 +1,29 @@
-import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
-import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
-import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
-import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
+import env from '../../config/env'
 import { Controller } from '../../../presentation/protocols'
-import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { makeSignInValidation } from './signin-validation-factory'
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
+import { SignInController } from '../../../presentation/controllers/signin/signin-controller'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 
 export const makeSignInController = () : Controller => {
-	const bcryptAdapter = new BcryptAdapter()
+	const salt = 12
+	const bcryptAdapter = new BcryptAdapter(salt)
 	const accountMongoRepository = new AccountMongoRepository()
-	const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-	const signUpController = new SignUpController(dbAddAccount, makeSignInValidation())
+	const jwtAdapter = new JwtAdapter(env.jwtSecret)
+
+	const dbAuthentication = new DbAuthentication(
+		accountMongoRepository,
+		bcryptAdapter,
+		jwtAdapter,
+		accountMongoRepository
+	)
+
+	const signInController = new SignInController(dbAuthentication, makeSignInValidation())
 	const logMongoRepository = new LogMongoRepository()
 
-	return new LogControllerDecorator(signUpController, logMongoRepository)
+	return new LogControllerDecorator(signInController, logMongoRepository)
 }
