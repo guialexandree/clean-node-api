79cc5c8e22576de7aab196df1b7099ddf3d4ab35
guialexandre <guilherme_alexandree@hotmail.com>
2022-10-13 21:14:23 -0300

    test: ensure DbLoadAccountBytToken return an account on success

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -87,4 +87,12 @@ describe('DbLoadAccountByToken UseCase', () => {
 
 		expect(account).toBeNull()
 	})
+
+	test('Should return an account on success', async () => {
+		const { sut } = makeSut()
+
+		const account = await sut.load('any_token', 'any_role')
+
+		expect(account).toEqual(makeFakeAccount())
+	})
 })
