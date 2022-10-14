730d0ef6ea7756bb8734e73b8d917b772d869e0d
guialexandre <guilherme_alexandree@hotmail.com>
2022-10-13 21:45:13 -0300

    test: ensure JwtAdapter return a value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -56,5 +56,13 @@ describe('JWT Adapter', () => {
 
 			expect(verifySpy).toHaveBeenCalledWith('any_token', 'any_secret')
 		})
+
+		test('Should return a value on verify success', async () => {
+			const sut = makeSut()
+
+			const value = await sut.decrypt('any_token')
+
+			expect(value).toEqual('any_value')
+		})
 	})
 })
