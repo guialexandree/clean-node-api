fa7c37b9e2c60c3b626f5bb2776456a357643f7f
guialexandre <guilherme_alexandree@hotmail.com>
2022-10-13 22:09:37 -0300

    test: ensure AccountMongoRepository find account by token

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -104,4 +104,22 @@ describe('Account Mongo Repository', () => {
 			void expect(promise).rejects.toThrow()
 		})
 	})
+
+	describe('loadAccountByToken()', () => {
+		test('Should return account on loadAccountByToken success', async () => {
+			const sut = makeSut()
+
+			await accountCollection.insertOne({
+				...makeFakeAccount(),
+				accessToken: 'any_token'
+			})
+			const account = await sut.loadByToken('any_token')
+
+			expect(account).toBeTruthy()
+			expect(account.id).toBeTruthy()
+			expect(account.name).toBe('any_name')
+			expect(account.email).toBe('any_email')
+			expect(account.password).toBe('any_password')
+		})
+	})
 })
