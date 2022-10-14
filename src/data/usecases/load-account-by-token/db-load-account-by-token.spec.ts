a122f2f4345528a8d8a3d6c79dd910c1dc16dc2c
guialexandre <guilherme_alexandree@hotmail.com>
2022-10-13 21:17:18 -0300

    test: ensure DbLoadAccountByToken returns throw if depencencies throws

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -95,4 +95,26 @@ describe('DbLoadAccountByToken UseCase', () => {
 
 		expect(account).toEqual(makeFakeAccount())
 	})
+
+	test('Should throw if LoadAccountByTokenRepository throws', () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    jest
+      .spyOn(loadAccountByTokenRepositoryStub, 'loadByToken')
+      .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+
+    const promise = sut.load('any_token', 'any_role')
+
+    void expect(promise).rejects.toThrow()
+  })
+
+	test('Should throw if Decrypter throws', () => {
+    const { sut, decrypterStub } = makeSut()
+    jest
+      .spyOn(decrypterStub, 'decrypt')
+      .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+
+    const promise = sut.load('any_token', 'any_role')
+
+    void expect(promise).rejects.toThrow()
+  })
 })
