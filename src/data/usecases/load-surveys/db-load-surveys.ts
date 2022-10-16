0352844d903e5967763546439ea504ce610da2ab
guialexandre <guilherme_alexandree@hotmail.com>
2022-10-15 21:33:06 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,14 @@
+import { LoadSurveys } from '@/domain/usecases/load-surveys'
+import { SurveyModel } from '@/domain/models/survey'
+import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository'
+
+export class DbLoadSurveys implements LoadSurveys {
+	constructor (
+		private readonly loadSurveysRepository: LoadSurveysRepository
+	) {}
+
+	async load (): Promise<SurveyModel[]> {
+		await this.loadSurveysRepository.loadAll()
+		return null
+	}
+}
