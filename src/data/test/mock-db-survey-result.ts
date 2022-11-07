6e0fbfdb64b4b5355d2fcdee56a3dddf4535f595
guialexandre <guilherme_alexandree@hotmail.com>
2022-11-06 21:40:03 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyResulRepository with correct values

--- src/data/test/mock-db-survey-result.ts
+++ src/data/test/mock-db-survey-result.ts
@@ -2,6 +2,7 @@ import { mockSurveyResultModel } from '@/domain/test'
 import { SurveyResultModel } from '@/domain/models/survey-result'
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result'
 import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository'
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 
 export const mockSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
   class DbSaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
@@ -10,4 +11,14 @@ export const mockSaveSurveyResultRepository = (): SaveSurveyResultRepository => 
     }
   }
   return new DbSaveSurveyResultRepositoryStub()
+}
+
+export const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
+	class DbLoadSurveyResultStub implements LoadSurveyResultRepository {
+		async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+			return await Promise.resolve(mockSurveyResultModel())
+		}
+	}
+
+	return new DbLoadSurveyResultStub()
 }
---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -0,0 +1,12 @@
+import { LoadSurveyResult, SurveyResultModel, LoadSurveyResultRepository } from './db-load-survey-result-protocols'
+
+export class DbLoadSurveyResult implements LoadSurveyResult {
+	constructor (
+		private readonly loadSurveyResultRepository: LoadSurveyResultRepository
+	) {}
+
+	async load (surveyId: string): Promise<SurveyResultModel> {
+		await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+		return null
+	}
+}
