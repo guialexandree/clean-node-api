aee8000a2ff1584574f72dd15c69abf73842145d
guialexandre <guilherme_alexandree@hotmail.com>
2022-11-06 22:04:04 -0300

    feat: ensure SurveyResultMongoRepository loads surveyResult correctly

--- src/infra/db/mongodb/save-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/save-result/survey-result-mongo-repository.ts
@@ -2,8 +2,9 @@ import { SaveSurveyResultRepository, SurveyResultModel, SaveSurveyResultParams }
 import { MongoHelper } from '../helpers/mongo-helper'
 import { ObjectId } from 'mongodb'
 import { QueryBuilder } from '../helpers'
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 
-export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository, LoadSurveyResultRepository {
 	async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
 		const surveyCollectionCollection = await MongoHelper.getCollection('surveyResults')
     await surveyCollectionCollection.findOneAndUpdate({
@@ -21,7 +22,7 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
     return surveyResults
 	}
 
-	private async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+	public async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
 		const surveyCollectionCollection = await MongoHelper.getCollection('surveyResults')
     const query = new QueryBuilder()
 			.match({
