ffe84d829b3b744fa80cb92b505b312df0479748
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-11-12 11:01:56 -0300

    feat: add to serve static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import setupStaticFiles from './static-files'
 import setupSwagger from './config-swagger'
 
 const app = express()
 
 setupSwagger(app)
+setupStaticFiles(app)
 setupMiddlewares(app)
 setupRoutes(app)
 
