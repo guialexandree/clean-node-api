7b2d3caac9a87e593d0a27a3e8c1325e584f0dff
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-11-27 13:14:51 -0300

    test: ensure SignUp Mutation returns EmailInUseError on invalid data

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -58,4 +58,40 @@ describe('Login GraphQL', () => {
       expect(res.body.errors[0].message).toBe('Unauthorized')
 		})
 	})
+
+	describe('SignUp Mutation', () => {
+    const query = `mutation {
+      signUp (name: "Guilherme Alexandre", email: "guilherme_alexandre@hotmail.com", password: "123", passwordConfirmation: "123") {
+        accessToken
+        name
+      }
+    }`
+
+    test('Should return an Account on valid data', async () => {
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+
+			expect(res.status).toBe(200)
+      expect(res.body.data.signUp.accessToken).toBeTruthy()
+      expect(res.body.data.signUp.name).toBe('Guilherme Alexandre')
+    })
+
+		test('Should return EmailInUseError on invalid data', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Guilherme Alexandre',
+        email: 'guilherme_alexandre@hotmail.com',
+        password
+      })
+
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+
+      expect(res.status).toBe(403)
+      expect(res.body.data).toBeFalsy()
+      expect(res.body.errors[0].message).toBe('The received email is already in use')
+    })
+  })
 })
