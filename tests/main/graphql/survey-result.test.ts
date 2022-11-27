600f4e999d21d7b8fbfaaa5be2a11a4c5c11ec65
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-11-27 13:49:38 -0300

    test: ensure SaveSurveyResult Mutation AccessDeniedError if no token is provided

--- tests/main/graphql/survey-result.test.ts
+++ tests/main/graphql/survey-result.test.ts
@@ -176,5 +176,36 @@ describe('SurveyResult GraphQL', () => {
         isCurrentAccountAnswer: false
       }])
     })
+
+		test('Should return AccessDeniedError if no token is provided', async () => {
+      const surveyRes = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      const query = `mutation {
+        saveSurveyResult (surveyId: "${surveyRes.insertedId.toHexString()}", answer: "Answer 1") {
+          question
+          answers {
+            answer
+            count
+            percent
+            isCurrentAccountAnswer
+          }
+          date
+        }
+      }`
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+      expect(res.status).toBe(403)
+      expect(res.body.data).toBeFalsy()
+      expect(res.body.errors[0].message).toBe('Access denied')
+    })
 	})
 })
